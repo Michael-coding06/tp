5ac9c8b9f06f1a279be38a7b616084131d5fe057
wn <lauweina@gmail.com>
2026-03-11 16:28:34 +0800

    Add main class ClassMate

---
+++ src/main/java/ClassMate.java
@@ -0,0 +1,6 @@
+public class ClassMate {
+
+    public static void main (String args[]) {
+        System.out.println("ClassMate class serves as the main entry point for the CLI-based application ClassMate.");
+    }
+}
