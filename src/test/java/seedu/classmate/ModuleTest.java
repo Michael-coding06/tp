90b586577d4b73d8426e949070fd6c96fe3d3b53
Neeraj <neerajjrsgp@gmail.com>
2026-03-12 21:05:39 +0800

    Add JUnit tests for Module class

--- src/test/java/seedu/classmate/ModuleTest.java
+++ src/test/java/seedu/classmate/ModuleTest.java
@@ -1,37 +1,40 @@
 package seedu.classmate;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
-
 /**
  * Contains unit tests for the Module class.
  * Tests for empty module codes and whether all modules are unique.
  */
 class ModuleTest {
-
     @Test
     void addPrerequisite_duplicateInput_noChange() {
         Module module = new Module("CG1111A", "Engineering Principles and Practice I");
         module.addPrerequisite("CS1010");
-        module.addPrerequisite("CS1010"); // Duplicate code
-
-        /*
-         * Since prerequisites are stored as an ArrayList<String>,
-         * Size of the ArrayList will increment without detecting duplicate entry
-         */
+        module.addPrerequisite("CS1010");
         assertEquals(1, module.getPrerequisites().size(), "Prerequisite module should not be duplicated.");
     }
-
     @Test
     void module_nullName_throwsException() {
-        /*
-         * Throw custom exception for null string input
-         */
         ClassMateException e = assertThrows(ClassMateException.class, () -> {
             new Module("", "");
         });
-
         assertEquals("Module details cannot be empty.", e.getMessage());
+    }
+    @Test
+    void toString_validModule_returnsCorrectString() {
+        Module module = new Module("CS2113", "Software Engineering & OOP");
+        assertEquals("CS2113 Software Engineering & OOP", module.toString());
+    }
+    @Test
+    void toStringPrerequisites_noPrerequisites_returnsNoPrereqMessage() {
+        Module module = new Module("CS1010", "Programming Methodology");
+        assertEquals("CS1010 has no prerequisites.", module.toStringPrerequisites());
+    }
+    @Test
+    void addPrerequisites_multipleInputs_allAdded() {
+        Module module = new Module("CS2113", "Software Engineering & OOP");
+        module.addPrerequisites("CS2040C", "CS1010");
+        assertEquals(2, module.getPrerequisites().size());
     }
 }
